3ec5218bc14cabf5f1c317b4924ff9d93fa9b2ea
zed <zedsmail@163.com>
2017-10-22 16:29:15 +0800

    init

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$BOS_TEST_CONFIG = [
+    'credentials' => array(
+        'accessKeyId' => '',
+        'secretAccessKey' => '',
+        'sessionToken' => ''
+    ),
+    'endpoint' => 'http://gz.bcebos.com',
+    'stsEndpoint' => '',
+];
